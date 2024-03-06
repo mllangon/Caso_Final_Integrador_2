ffa81888831259162328b713c112e72a730e8632
mllangon <mllangon@myuax.com>
2024-03-06 17:49:41 +0100

    17

--- src/main/java/Animales/Animales.java
+++ src/main/java/Animales/Animales.java
@@ -15,8 +15,50 @@ public abstract class Animales {
         this.comportamiento = comportamiento;
     }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getSalud() {
+        return salud;
+    }
+
+    public void setSalud(String salud) {
+        this.salud = salud;
+    }
+
+    public String getAlimentacion() {
+        return alimentacion;
+    }
+
+    public void setAlimentacion(String alimentacion) {
+        this.alimentacion = alimentacion;
+    }
+
+    public String getComportamiento() {
+        return comportamiento;
+    }
+
+    public void setComportamiento(String comportamiento) {
+        this.comportamiento = comportamiento;
+    }
+
     public abstract void comer();
     public abstract void dormir();
     public abstract void moverse();
     public abstract void comportarse();
+
+
 }

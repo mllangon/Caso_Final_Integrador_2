d5c16bf695761753579bc5131bf000aa777c655b
mllangon <mllangon@myuax.com>
2024-03-06 17:02:48 +0100

    7

--- src/main/java/Animales/Animales.java
+++ src/main/java/Animales/Animales.java
@@ -1,4 +1,22 @@
 package Animales;
 
-public class Animales {
+public abstract class Animales {
+    protected String nombre;
+    protected int edad;
+    protected String salud;
+    protected String alimentacion;
+    protected String comportamiento;
+
+    public Animales(String nombre, int edad, String salud, String alimentacion, String comportamiento) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.salud = salud;
+        this.alimentacion = alimentacion;
+        this.comportamiento = comportamiento;
+    }
+
+    public abstract void comer();
+    public abstract void dormir();
+    public abstract void moverse();
+    public abstract void comportarse();
 }

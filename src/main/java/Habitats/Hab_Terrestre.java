3346e550e10a58f2cd544e350b2456fd1e2c2056
mllangon <mllangon@myuax.com>
2024-03-06 16:44:14 +0100

    7

--- src/main/java/Habitats/Hab_Terrestre.java
+++ src/main/java/Habitats/Hab_Terrestre.java
@@ -1,4 +1,38 @@
 package Habitats;
 
-public class Hab_Terrestre {
+public class Hab_Terrestre extends Habitat {
+    private String tipoSuelo;
+    private String vegetacion;
+    private boolean disponibilidadRefugio;
+
+    public Hab_Terrestre(double temperatura, double humedad, boolean limpieza, String tipoSuelo, String vegetacion, boolean disponibilidadRefugio) {
+        super(temperatura, humedad, limpieza);
+        this.tipoSuelo = tipoSuelo;
+        this.vegetacion = vegetacion;
+        this.disponibilidadRefugio = disponibilidadRefugio;
+    }
+
+    public String getTipoSuelo() {
+        return tipoSuelo;
+    }
+
+    public void setTipoSuelo(String tipoSuelo) {
+        this.tipoSuelo = tipoSuelo;
+    }
+
+    public String getVegetacion() {
+        return vegetacion;
+    }
+
+    public void setVegetacion(String vegetacion) {
+        this.vegetacion = vegetacion;
+    }
+
+    public boolean isDisponibilidadRefugio() {
+        return disponibilidadRefugio;
+    }
+
+    public void setDisponibilidadRefugio(boolean disponibilidadRefugio) {
+        this.disponibilidadRefugio = disponibilidadRefugio;
+    }
 }

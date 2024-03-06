b5810a221d6917969669b40a2c8c1399968616ad
mllangon <mllangon@myuax.com>
2024-03-06 17:44:11 +0100

    16

--- src/main/java/Animales/Anfibios.java
+++ src/main/java/Animales/Anfibios.java
@@ -1,4 +1,28 @@
 package Animales;
 
-public class Anfibios {
+public class Anfibios extends Animales {
+
+    public Anfibios(String nombre, int edad, String salud, String alimentacion, String comportamiento) {
+        super(nombre, edad, salud, alimentacion, comportamiento);
+    }
+
+    @Override
+    public void comer() {
+        System.out.println(nombre + " está comiendo. Dieta: " + alimentacion);
+    }
+
+    @Override
+    public void dormir() {
+        System.out.println(nombre + " está durmiendo.");
+    }
+
+    @Override
+    public void moverse() {
+        System.out.println(nombre + " está saltando.");
+    }
+
+    @Override
+    public void comportarse() {
+        System.out.println("Comportamiento de " + nombre + ": " + comportamiento);
+    }
 }

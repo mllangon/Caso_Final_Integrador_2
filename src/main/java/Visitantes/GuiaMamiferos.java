84c2ce4fbfbedd22d3cc2a8620983fe87387bacf
mllangon <mllangon@myuax.com>
2024-03-06 18:38:35 +0100

    22

--- src/main/java/Visitantes/GuiaMamiferos.java
+++ src/main/java/Visitantes/GuiaMamiferos.java
@@ -1,4 +1,8 @@
 package Visitantes;
 
-public class GuiaMamiferos {
+public class GuiaMamiferos extends Guia {
+    @Override
+    public void ofrecerGuia() {
+        System.out.println("Bienvenido al tour de mamíferos. Aquí podrás ver diferentes tipos de mamíferos y aprender sobre ellos.");
+    }
 }

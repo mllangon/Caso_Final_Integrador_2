9b424932ebf0500c8a845925a136eab57b855d7e
mllangon <mllangon@myuax.com>
2024-03-06 11:27:09 +0100

    3

--- src/main/java/Habitats/Hab_Acuatico.java
+++ src/main/java/Habitats/Hab_Acuatico.java
@@ -1,4 +1,18 @@
 package Habitats;
 
-public class Hab_Acuatico {
+public class Hab_Acuatico extends Habitat {
+    private double nivelAgua;
+
+    public Hab_Acuatico(double temperatura, double humedad, boolean limpieza, double nivelAgua) {
+        super(temperatura, humedad, limpieza);
+        this.nivelAgua = nivelAgua;
+    }
+
+    public double getNivelAgua() {
+        return nivelAgua;
+    }
+
+    public void setNivelAgua(double nivelAgua) {
+        this.nivelAgua = nivelAgua;
+    }
 }

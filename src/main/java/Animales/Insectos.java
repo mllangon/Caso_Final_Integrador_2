b82e0e8b68e9a876f4f5ae8033d9707c951ecb5c
mllangon <mllangon@myuax.com>
2024-03-06 17:35:32 +0100

    13

--- src/main/java/Animales/Insectos.java
+++ src/main/java/Animales/Insectos.java
@@ -1,4 +1,28 @@
 package Animales;
 
-public class Insectos {
+public class Insectos extends Animales {
+
+    public Insectos(String nombre, int edad, String salud, String alimentacion, String comportamiento) {
+        super(nombre, edad, salud, alimentacion, comportamiento);
+    }
+
+    @Override
+    public void comer() {
+        System.out.println(nombre + " está comiendo. Dieta: " + alimentacion);
+    }
+
+    @Override
+    public void dormir() {
+        System.out.println(nombre + " está durmiendo.");
+    }
+
+    @Override
+    public void moverse() {
+        System.out.println(nombre + " se está moviendo.");
+    }
+
+    @Override
+    public void comportarse() {
+        System.out.println("Comportamiento de " + nombre + ": " + comportamiento);
+    }
 }

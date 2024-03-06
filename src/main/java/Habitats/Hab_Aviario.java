f88e2d66ed7213d6a1ea64f1a863b931440eb763
mllangon <mllangon@myuax.com>
2024-03-06 11:38:31 +0100

    5

--- src/main/java/Habitats/Hab_Aviario.java
+++ src/main/java/Habitats/Hab_Aviario.java
@@ -1,4 +1,28 @@
 package Habitats;
 
-public class Hab_Aviario {
+public class Hab_Aviario extends Habitat {
+    private double altura;
+    private int numeroDeNidos;
+
+    public Hab_Aviario(double temperatura, double humedad, boolean limpieza, double altura, int numeroDeNidos) {
+        super(temperatura, humedad, limpieza);
+        this.altura = altura;
+        this.numeroDeNidos = numeroDeNidos;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public int getNumeroDeNidos() {
+        return numeroDeNidos;
+    }
+
+    public void setNumeroDeNidos(int numeroDeNidos) {
+        this.numeroDeNidos = numeroDeNidos;
+    }
 }

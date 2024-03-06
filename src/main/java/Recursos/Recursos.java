39147e28140a683f5d72340370e914a517f3c610
mllangon <mllangon@myuax.com>
2024-03-06 19:00:19 +0100

    23

---
+++ src/main/java/Recursos/Recursos.java
@@ -0,0 +1,38 @@
+package Recursos;
+
+
+public class Recursos {
+    private String nombre;
+    private String tipo;
+    private int cantidad;
+
+    public Recursos(String nombre, String tipo, int cantidad) {
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.cantidad = cantidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+}

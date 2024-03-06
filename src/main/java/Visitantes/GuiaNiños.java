6b87e38af309d856d058b4c62b74a655f45f93ed
mllangon <mllangon@myuax.com>
2024-03-06 18:38:03 +0100

    21

--- src/main/java/Visitantes/GuiaNiños.java
+++ src/main/java/Visitantes/GuiaNiños.java
@@ -1,4 +1,8 @@
 package Visitantes;
 
-public class GuiaNiños {
+public class GuiaNiños extends Guia {
+    @Override
+    public void ofrecerGuia() {
+        System.out.println("Bienvenido al tour para niños. Aquí podrás ver animales divertidos y aprender sobre ellos de una manera divertida.");
+    }
 }

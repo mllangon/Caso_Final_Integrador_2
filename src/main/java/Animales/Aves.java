759d5a0bf525426d3eda5e917e1de53c1bfafc4f
mllangon <mllangon@myuax.com>
2024-03-06 17:26:46 +0100

    13

--- src/main/java/Animales/Aves.java
+++ src/main/java/Animales/Aves.java
@@ -1,4 +1,29 @@
 package Animales;
 
-public class Aves {
+public class Aves extends Animales {
+
+
+    public Aves(String nombre, int edad, String salud, String alimentacion, String comportamiento, String tipoPlumas) {
+        super(nombre, edad, salud, alimentacion, comportamiento);
+    }
+
+    @Override
+    public void comer() {
+        System.out.println(nombre + " está comiendo. Dieta: " + alimentacion);
+    }
+
+    @Override
+    public void dormir() {
+        System.out.println(nombre + " está durmiendo.");
+    }
+
+    @Override
+    public void moverse() {
+        System.out.println(nombre + " está volando.");
+    }
+
+    @Override
+    public void comportarse() {
+        System.out.println("Comportamiento de " + nombre + ": " + comportamiento);
+    }
 }

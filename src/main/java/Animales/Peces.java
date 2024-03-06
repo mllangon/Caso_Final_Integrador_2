0466a100e60455fe1c8d3e32a62f2093dc977790
mllangon <mllangon@myuax.com>
2024-03-06 17:37:38 +0100

    15

--- src/main/java/Animales/Peces.java
+++ src/main/java/Animales/Peces.java
@@ -1,4 +1,28 @@
 package Animales;
 
-public class Peces {
+public class Peces extends Animales {
+
+    public Peces(String nombre, int edad, String salud, String alimentacion, String comportamiento) {
+        super(nombre, edad, salud, alimentacion, comportamiento);
+    }
+
+    @Override
+    public void comer() {
+        System.out.println(nombre + " está comiendo. Dieta: " + alimentacion);
+    }
+
+    @Override
+    public void dormir() {
+        System.out.println(nombre + " está durmiendo.");
+    }
+
+    @Override
+    public void moverse() {
+        System.out.println(nombre + " está nadando.");
+    }
+
+    @Override
+    public void comportarse() {
+        System.out.println("Comportamiento de " + nombre + ": " + comportamiento);
+    }
 }

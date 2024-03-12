e5f1f8eaa986da715e451492ed92c623fb6ee8e8
mllangon <mllangon@myuax.com>
2024-03-12 17:00:22 +0100

    25

--- src/main/java/Mantenimiento/Seguridad.java
+++ src/main/java/Mantenimiento/Seguridad.java
@@ -1,4 +1,40 @@
 package Mantenimiento;
 
 public class Seguridad {
+    private String nombre;
+    private String tipo;
+    private String ubicacion;
+    private String estado;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
 }

e9ef9e3f70e7045312442f6609bb200c775d2d18
mllangon <mllangon@myuax.com>
2024-03-12 17:01:40 +0100

    25

--- src/main/java/Mantenimiento/RegistroAcceso.java
+++ src/main/java/Mantenimiento/RegistroAcceso.java
@@ -1,4 +1,40 @@
 package Mantenimiento;
 
 public class RegistroAcceso {
+    private String usuario;
+    private String horaEntrada;
+    private String horaSalida;
+    private String ubicacion;
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getHoraEntrada() {
+        return horaEntrada;
+    }
+
+    public void setHoraEntrada(String horaEntrada) {
+        this.horaEntrada = horaEntrada;
+    }
+
+    public String getHoraSalida() {
+        return horaSalida;
+    }
+
+    public void setHoraSalida(String horaSalida) {
+        this.horaSalida = horaSalida;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
 }

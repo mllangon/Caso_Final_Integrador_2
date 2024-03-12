6ad68951c83bc7fe1bff818c1d836262837d9946
mllangon <mllangon@myuax.com>
2024-03-12 18:05:17 +0100

    Animales

--- src/main/java/Animales/Anfibios.java
+++ src/main/java/Animales/Anfibios.java
@@ -1,6 +1,9 @@
 package Animales;
 
 public class Anfibios extends Animales {
+    public static final Anfibios RANA = new Anfibios("Rana", 2, "Saludable", "Insectos", "Saltar");
+    public static final Anfibios SALAMANDRA = new Anfibios("Salamandra", 3, "Saludable", "Insectos", "Caminar");
+    public static final Anfibios TRITON = new Anfibios("Tritón", 1, "Saludable", "Insectos", "Nadar");
 
     public Anfibios(String nombre, int edad, String salud, String alimentacion, String comportamiento) {
         super(nombre, edad, salud, alimentacion, comportamiento);

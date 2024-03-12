d13cbf21bcce4b5fae9963b27a552b5856b9807a
mllangon <mllangon@myuax.com>
2024-03-12 17:53:09 +0100

    Animales

--- src/main/java/Animales/Mamifero.java
+++ src/main/java/Animales/Mamifero.java
@@ -2,8 +2,24 @@ package Animales;
 
 public class Mamifero extends Animales {
 
+    public static final Mamifero LEON_M = new Mamifero("León macho", 7, "Saludable", "Carnívoro", "Agresivo", "Pelaje corto y dorado");
+    public static final Mamifero LEON_H = new Mamifero ("León hembra", 5, "Saludable", "Carnívoro", "Agresiva", "Pelaje corto y dorado");
+    public static final Mamifero TIGRE_M = new Mamifero("Tigre", 4, "Enfermo", "Carnívoro", "Agresivo", "Pelaje corto y a rayas");
+    public static final Mamifero TIGRE_H = new Mamifero("Tigresa", 3, "Saludable", "Carnívoro", "Agresiva", "Pelaje corto y a rayas");
+    public static final Mamifero OSO = new Mamifero("Oso", 10, "Saludable", "Omnívoro", "Pacífico", "Pelaje largo y marrón");
+    public static final Mamifero CHIMPANCE_P = new Mamifero("Chimpancé madre", 12, "Saludable", "Omnívoro", "Sociable", "Pelaje corto y negro");
+    public static final Mamifero CHIMPANCE_H = new Mamifero("Chimpancé cría", 2, "Saludable", "Omnívoro", "Sociable", "Pelaje corto y negro");
+    public static final Mamifero JIRAFA_M = new Mamifero("Jirafa macho", 7, "Saludable", "Herbívoro", "Pacífico", "Pelaje manchado");
+    public static final Mamifero JIRAFA_H = new Mamifero("Jirafa hembra", 9, "Enferma", "Herbíbora", "Sociable" ,"Pelaje manchado");
+
     public Mamifero(String nombre, int edad, String salud, String alimentacion, String comportamiento, String tipoPelaje) {
         super(nombre, edad, salud, alimentacion, comportamiento);
+    }
+
+    private String tipoPelaje;
+
+    public String getTipoPelaje() {
+        return tipoPelaje;
     }
 
     @Override
@@ -24,5 +40,9 @@ public class Mamifero extends Animales {
     @Override
     public void comportarse() {
         System.out.println("Comportamiento de " + nombre + ": " + comportamiento);
+    }
+
+    public void tipoPelaje() {
+        System.out.println("Tipo de pelaje de " + nombre + ": " + tipoPelaje);
     }
 }

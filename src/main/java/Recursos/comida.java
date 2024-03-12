2fac0aa52f060dc346a9bdfe830d3436ecf204ce
mllangon <mllangon@myuax.com>
2024-03-12 21:02:47 +0100

    Main

--- src/main/java/Recursos/comida.java
+++ src/main/java/Recursos/comida.java
@@ -1,4 +1,21 @@
 package Recursos;
 
-public class comida {
+public class comida extends Recursos {
+    public static final comida CARNE = new comida("Carne", "Comida", 1000, "Carne");
+    public static final comida PESCADO = new comida("Pescado", "Comida", 1000, "Pescado");
+    public static final comida FRUTA = new comida("Fruta", "Comida", 1000, "Fruta");
+    public String tipoComida;
+
+    public comida(String nombre, String tipo, int cantidad, String tipoComida) {
+        super(nombre, tipo, cantidad);
+        this.tipoComida = tipoComida;
+    }
+
+    public String getTipoComida() {
+        return tipoComida;
+    }
+
+    public void setTipoComida(String tipoComida) {
+        this.tipoComida = tipoComida;
+    }
 }
--- src/main/java/Recursos/juguetes.java
+++ src/main/java/Recursos/juguetes.java
@@ -1,6 +1,9 @@
 package Recursos;
 
 public class juguetes extends Recursos {
+    public static final juguetes RUEDA = new juguetes("Rueda", "Juguete", 1000, "Rueda");
+    public static final juguetes PELOTA = new juguetes("Pelota", "Juguete", 1000, "Pelota");
+    public static final juguetes CUERDA = new juguetes("Cuerda", "Juguete", 1000, "Cuerda");
     public String tipoJuguete;
 
     public juguetes(String nombre, String tipo, int cantidad, String tipoJuguete) {

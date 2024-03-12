39c80f1e25002fd40ac9dcc3ba44d10546070511
mllangon <mllangon@myuax.com>
2024-03-12 18:01:53 +0100

    Animales

--- src/main/java/Animales/Insectos.java
+++ src/main/java/Animales/Insectos.java
@@ -1,7 +1,11 @@
 package Animales;
 
 public class Insectos extends Animales {
-
+public static final Insectos VIUDA_NEGRA = new Insectos("Viuda negra", 1, "Saludable", "Carnívoro", "Calmada");
+public static final Insectos MARIPOSA_R = new Insectos("Mariposa rosa", 1, "Saludable", "Herbívoro", "Sociable");
+public static final Insectos MARIPOSA_A = new Insectos("Mariposa azul", 2, "Saludable", "Herbívoro", "Sociable");
+public static final Insectos ESCARABAJO = new Insectos("Escarabajo", 4, "Saludable", "Herbívoro", "Curioso");
+public static final Insectos HORMIGA_B = new Insectos("Hormiga bala", 1, "Saludable", "Herbívoro", "Trabajadora");
 
     public Insectos(String  nombre, int edad, String salud, String alimentacion, String comportamiento) {
         super(nombre, edad, salud, alimentacion, comportamiento);

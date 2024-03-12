5294066468362283a896177bb1c5dfa25d4bae29
mllangon <mllangon@myuax.com>
2024-03-12 20:15:11 +0100

    Main

--- src/main/java/Habitats/Hab_Aviario.java
+++ src/main/java/Habitats/Hab_Aviario.java
@@ -1,6 +1,10 @@
 package Habitats;
 
 public class Hab_Aviario extends Habitat {
+    public static final Hab_Aviario ZONA_1 = new Hab_Aviario(25, 60, true, 5, 3);
+    public static final Hab_Aviario ZONA_2 = new Hab_Aviario(23, 70, false, 10, 2);
+    public static final Hab_Aviario ZONA_3 = new Hab_Aviario(20, 50, true, 15, 5);
+    public static final Hab_Aviario ZONA_4 = new Hab_Aviario(27, 60, false, 20, 1);
     private double altura;
     private int numeroDeNidos;
 
--- src/main/java/Habitats/Hab_Terrestre.java
+++ src/main/java/Habitats/Hab_Terrestre.java
@@ -1,14 +1,14 @@
 package Habitats;
 
 public class Hab_Terrestre extends Habitat {
-    static final Hab_Terrestre ZONA_1 = new Hab_Terrestre(25, 80, true, "Tierra", "Sabana", true);
-    static final Hab_Terrestre ZONA_2 = new Hab_Terrestre(30, 70, true, "Tierra", "Jungla", true);
-    static final Hab_Terrestre ZONA_3 = new Hab_Terrestre(33, 60, false, "Tierra", "Montaña", true);
-    static final Hab_Terrestre ZONA_4 = new Hab_Terrestre(27, 50, true, "Tierra", "Jungla", true);
-    static final Hab_Terrestre ZONA_5 = new Hab_Terrestre(30, 40, true, "Tierra", "Sabana", true);
-    private String tipoSuelo;
-    private String vegetacion;
-    private boolean disponibilidadRefugio;
+    public static final Hab_Terrestre ZONA_1 = new Hab_Terrestre(25, 80, true, "Tierra", "Sabana", true);
+    public static final Hab_Terrestre ZONA_2 = new Hab_Terrestre(30, 70, true, "Tierra", "Jungla", true);
+    public static final Hab_Terrestre ZONA_3 = new Hab_Terrestre(33, 60, false, "Tierra", "Montaña", true);
+    public static final Hab_Terrestre ZONA_4 = new Hab_Terrestre(27, 50, true, "Tierra", "Jungla", true);
+    public static final Hab_Terrestre ZONA_5 = new Hab_Terrestre(30, 40, true, "Tierra", "Sabana", true);
+    public String tipoSuelo;
+    public String vegetacion;
+    public boolean disponibilidadRefugio;
 
     public Hab_Terrestre(double temperatura, double humedad, boolean limpieza, String tipoSuelo, String vegetacion, boolean disponibilidadRefugio) {
         super(temperatura, humedad, limpieza);

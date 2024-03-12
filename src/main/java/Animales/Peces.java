21825eaf659dcd1cb35188214f18193136fa5425
mllangon <mllangon@myuax.com>
2024-03-12 18:11:22 +0100

    Animales

--- src/main/java/Animales/Peces.java
+++ src/main/java/Animales/Peces.java
@@ -1,6 +1,12 @@
 package Animales;
 
 public class Peces extends Animales {
+    public static final Peces TIBURON = new Peces("Tiburón", 5, "Saludable", "Carnívoro", "Agresivo");
+    public static final Peces PEZ_PAYASO = new Peces("Pez payaso", 2, "Herido", "Carnívoro", "Calmado");
+    public static final Peces PEZ_GLOBO = new Peces("Pez globo", 1, "Saludable", "Carnívoro", "Sociable");
+    public static final Peces PEZ_CIRUJANO = new Peces("Pez cirujano", 3, "Enfermo", "Carnívoro", "Tranquilo");
+    public static final Peces PEZ_MANDARIN = new Peces("Pez mandarín", 2, "Saludable", "Carnívoro", "Posesivo");
+    public static final Peces DELFIN = new Peces("Delfín", 10, "Saludable", "Carnívoro", "Juguetón");
 
     public Peces(String nombre, int edad, String salud, String alimentacion, String comportamiento) {
         super(nombre, edad, salud, alimentacion, comportamiento);
--- src/main/java/Animales/Reptiles.java
+++ src/main/java/Animales/Reptiles.java
@@ -1,6 +1,12 @@
 package Animales;
 
 public class Reptiles extends Animales {
+    public static final Reptiles COCODRILO = new Reptiles("Cocodrilo", 10, "Saludable", "Carnívoro", "Agresivo");
+    public static final Reptiles SERPIENTE = new Reptiles("Serpiente", 5, "Saludable", "Carnívoro", "Sigilosa");
+    public static final Reptiles LAGARTIJA = new Reptiles("Lagartija", 2, "Saludable", "Omnívoro", "Miedosa");
+    public static final Reptiles TORTUGA_M = new Reptiles("Tortuga macho", 75, "Saludable", "Herbívoro", "Lenta");
+    public static final Reptiles TORTUGA_H = new Reptiles("Tortuga hembra", 63, "Saludable", "Herbívoro", "Lenta");
+    public static final Reptiles CAMALEON = new Reptiles("Camaleón", 3, "Saludable", "Omnívoro", "Camuflaje");
 
     public Reptiles(String nombre, int edad, String salud, String alimentacion, String comportamiento) {
         super(nombre, edad, salud, alimentacion, comportamiento);
